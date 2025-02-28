198f8e499156ae46b703f8c5d465cb61a20517e0
12916 <12916@hageveld.nl>
2025-02-28 17:26:34 +0100

    Hoofdscript verbeterd met elk script tot nu toe

--- parkour_functies.py
+++ parkour_functies.py
@@ -5,6 +5,7 @@ HEIGHT = 690
 gravity = 0.6
 screen = pygame.display.set_mode((WIDTH, HEIGHT), RESIZABLE)
 
+
 class Objects:
     def __init__(self, xpos, ypos, width, height, color, mass, xspeed, yspeed):
         self.xpos = xpos
@@ -18,10 +19,10 @@ class Objects:
         self.Rect = pygame.Rect(self.xpos, self.ypos, self.width, self.height)
         self.on_ground = False
 
-    def update_pos(self, platforms):
-        self.xpos += self.xspeed
-        self.Rect.topleft = (self.xpos, self.ypos)
+    def update_pos(self, platforms, CameraPosx):
 
+        self.xpos += self.xspeed
+        self.Rect.topleft = (self.xpos - CameraPosx, self.ypos)
 
         for platform in platforms:
             if self.Rect.colliderect(platform.Rect):
@@ -30,17 +31,15 @@ class Objects:
                 elif self.xspeed < 0:
                     self.xpos = platform.xpos + platform.width
                 self.xspeed = 0
-                self.Rect.topleft = (self.xpos, self.ypos)
-
+                self.Rect.topleft = (self.xpos - CameraPosx, self.ypos)
 
         self.yspeed += self.mass * gravity
 
-
         self.ypos += self.yspeed
-        self.Rect.topleft = (self.xpos, self.ypos)
+        self.Rect.topleft = (self.xpos - CameraPosx, self.ypos)
         self.on_ground = False
 
-        #platformcollision
+        # platformcollision
         for platform in platforms:
             if self.Rect.colliderect(platform.Rect):
                 if self.yspeed > 0:  # Falling
@@ -50,22 +49,23 @@ class Objects:
                 elif self.yspeed < 0:  # Hitting ceiling
                     self.ypos = platform.ypos + platform.height
                     self.yspeed = 0
-                self.Rect.topleft = (self.xpos, self.ypos)
+                self.Rect.topleft = (self.xpos - CameraPosx, self.ypos)
+                return platform.color == "orange"
 
-        #wall en floor collision
+        # wall en floor collision
         if self.ypos + self.height >= HEIGHT - 4:
             self.ypos = HEIGHT - self.height - 4
             self.yspeed = 0
             self.on_ground = True
-            self.Rect.topleft = (self.xpos, self.ypos)
-        if self.xpos < 0:
-            self.xpos = 0
-        elif self.xpos + self.width > WIDTH:
+            self.Rect.topleft = (self.xpos - CameraPosx, self.ypos)
+        if self.xpos + self.width > WIDTH:
             self.xpos = WIDTH - self.width
-        self.Rect.topleft = (self.xpos, self.ypos)
+        self.Rect.topleft = (self.xpos - CameraPosx, self.ypos)
 
-    def draw(self, surface):
-        self.Rect = pygame.draw.rect(surface, self.color, (self.xpos, self.ypos, self.width, self.height))
+    def draw(self, surface, CameraPosx):
+        self.Rect = pygame.draw.rect(surface, self.color, (self.xpos - CameraPosx, self.ypos, self.width, self.height))
 
+
+# maakt vloer
 def draw_floor():
     pygame.draw.line(screen, (255, 255, 255), (0, HEIGHT), (WIDTH, HEIGHT), 25)
