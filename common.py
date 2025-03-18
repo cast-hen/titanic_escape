2a168eea49fb70be11ec986ce90b5a4121319a3f
brank <12840@hageveld.nl>
2025-03-18 11:52:11 +0100

    Added pydoc to more functions

--- common.py
+++ common.py
@@ -26,12 +26,12 @@ def eind():
 
 def textPrint(text, textSize, textColour, center):
     """
-    Prints the text on the screen. center is tuple.
-    :param text: What text will print
-    :param textSize: How big is the text
-    :param textColour: What color is the text
-    :param center: location of center text
-    :return: None
+    Prints the text on the screen.
+    text: string of text you want to display
+    textSize: the size of the text
+    textColour: the colour of the text
+    center: the center position of where the text should be placed
+    return: none
     """
     font = pygame.font.Font("freesansbold.ttf", textSize)
     text = font.render(text, True, textColour)
@@ -41,23 +41,20 @@ def textPrint(text, textSize, textColour, center):
 
 def game_over(lives, state=None):
     """
-    Shows the Game Over screen
-    :param lives: the amount of lives left of the player
-    :param state : The state of the game
-    :return: None
+    Function for if the player has died
+    lives: the ammount of lives the player has left
+    state: the current state of the FSM
+    return: the new ammount of lives and the new state of the FSM
     """
     lives -= 1
     screen.fill('red')
     if lives == 0:
         textPrint("Game over", 100, 'white', (WIDTH/2, HEIGHT/2 - 100))
         state = "Menu"
-        lives = 5
+        lives = 2
     else:
         textPrint("You died", 100, 'white', (WIDTH/2, HEIGHT/2))
-        if lives == 1:
-            textPrint("You have " + str(lives) + " life left", 40, 'white', (WIDTH / 2, HEIGHT / 2 + 100))
-        else:
-            textPrint("You have " + str(lives) + " lives left", 40, 'white', (WIDTH / 2, HEIGHT / 2 + 100))
+        textPrint("You have " + str(lives) + " live(s) left", 40, 'white', (WIDTH / 2, HEIGHT / 2 + 100))
     pygame.display.flip()
     time.sleep(2)
     return 200, 200, lives, state
@@ -65,8 +62,8 @@ def game_over(lives, state=None):
 
 def menu():
     """
-    Shows the menu screen
-    :return the pressed button (Start, Quit, Tuturial?):
+    the opening menu of the game
+    return: the action the player has made within the menu as a string
     """
     screen.fill('black')
     mouseDown = False
@@ -85,19 +82,3 @@ def menu():
             return "begin"
         if button.check(buttonQuit, mouse, mouseDown, screen):
             return "quit"
-def LevelGehaald():
-
-        """
-        Shows the level complete screen and moves on to the next level
-        :return none:
-        """
-
-        screen.fill((0, 0, 0))
-        textPrint("Level gehaald", 100, 'white', (WIDTH / 2, HEIGHT / 4))
-        textPrint("alle levens zijn hersteld", 100, 'white', (WIDTH / 2, HEIGHT / 4 + 300))
-        pygame.display.update()
-        time.sleep(3)
-
-
-
-
--- pauze.py
+++ pauze.py
@@ -2,18 +2,20 @@ import pygame
 from button_code import *
 from common import *
 
-
+state = 32832757342657832458324658734265234687342657834265873658234682346598234652
 screen = pygame.display.set_mode((1366, 690), pygame.RESIZABLE)
 
 def Pause():
     """
-    Shows the pauze screen
-    :return None:
+    Pauses the game until the player selects an option
+    return: the state the player should now be in
     """
     WIDTH = 1366
     HEIGHT = 690
-
-    mouseDown = False
+    resumeButton = button((WIDTH / 2 - 100), (HEIGHT / 2), 200, 80, 'grey', 'darkgrey', "resume", 'white', 50,
+                          'white')
+    menuButton = button((WIDTH / 2 - 100), (HEIGHT / 4 * 2.8), 200, 80, 'grey', 'darkgrey', "menu", 'white', 50,
+                        'white')
     dimSurface = pygame.Surface((WIDTH, HEIGHT))
     pygame.Surface.set_alpha(dimSurface, 150)
     pygame.Surface.blit(screen, dimSurface)
@@ -22,19 +24,15 @@ def Pause():
 
     while True:
         mouse = pygame.mouse.get_pos()
-
-        button1 = button((WIDTH / 2 - 100), (HEIGHT / 2), 200, 80, 'grey', 'darkgrey', "resume", 'white', 50,
-                              'white')
-        button2 = button((WIDTH / 2 - 100), (HEIGHT / 4 * 2.8), 200, 80, 'grey', 'darkgrey', "menu", 'white', 50,
-                            'white')
-        if button.check(button1, mouse, mouseDown, screen):
-            return None
-        if button.check(button2, mouse, mouseDown, screen):
-            screen.fill('black')
-            return "Menu"
+        mouseDown = False
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouseDown = True
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
-                    return None
+                    return state
+        if button.check(resumeButton, mouse, mouseDown, screen):
+            return state
+        if button.check(menuButton, mouse, mouseDown, screen):
+            screen.fill('black')
+            return "Menu"
