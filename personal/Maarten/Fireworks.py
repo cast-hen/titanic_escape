a4b67a0d4e1b78d6c9e276fdba7f7aa78c646d1e
12687 <12687@hageveld.nl>
2025-03-23 20:38:44 +0100

    Fireworks.py

--- personal/Maarten/Fireworks.py
+++ personal/Maarten/Fireworks.py
@@ -20,41 +20,37 @@ endColour = (40, 40, 40)
 pygame.display.set_caption("Fireworks")
 
 state = "Setup" # Setup, Rising, Exploding, Pause, Typing, Writing, End
-pointDensity = 2  # Higher values mean less particles thus faster
+pointDensity = 10  # Higher values mean less particles thus faster
 backgroundColour = (0, 0, 0)
 
 fireworkPhase = 0
-maxFireworkPhase = 25
+maxFireworkPhase = 50
 pointVelocity = 4 # pixels per frame
-explosionType = 1 # 0: Front point is the biggest. - 1: Points have random sizes
+explosionType = 0 # 0: Front point is the biggest. - 1: Points have random sizes
 
 totalExecutionTime = 0
-extraCharWidth = 5
 numberOfClicks = 0
-backspacePause = None
-h = fontSize + 2 * extraCharWidth
-setupSentence = "Type your word:    "
 
-centers = [(w//2, h//2)]
-r = 20
+numberOfFireworks = 6
+circleThickness = 10
 
+centerList = [(w/2, h/2)]
+radiusList = [200]
 
-def findPoints(centers, r):
+def findCirclePoints(centers, r):
     pointList = []
     numberOfPoints = 0
     for index in range(len(centers)):
         foundPoints = []
-        xCorner = centers[index][0] - r
-        yCorner = centers[index][1] - r
-        for pointY in range(yCorner, yCorner + 2 * r, pointDensity):
-            for pointX in range(xCorner, xCorner + 2 * r , pointDensity):
-                pointP = (pointX, pointY)
-                pixelColour = screen.get_at(pointP)[:3]
-                if pixelColour != backgroundColour:
-                    foundPoints.append(pointP)
+        for alpha in range(0, 360, 2):
+            angle = random.randint(0, 4) + alpha
+            deltaX = r[index] * math.sin(angle * math.pi / 180)
+            deltaY = r[index] * math.cos(angle * math.pi / 180)
+            point = (centers[index][0] - deltaX, centers[index][1] - deltaY)
+            foundPoints.append(point)
         pointList.append(foundPoints)
         numberOfPoints += len(foundPoints)
-    # print(pointList)
+    print(pointList)
     return pointList, numberOfPoints
 
 
@@ -63,16 +59,16 @@ def draw(pointList, centers):
         phase = maxFireworkPhase - len(colourGradients) - 1
     else:
         phase = fireworkPhase
-    for letter in range(len(centers)):
+    for firework in range(len(centers)):
         for p in range(phase, 0, -1):
             if state == "Rising":
-                xB = centers[letter][0]
-                yB = h - (h // (2 * (maxFireworkPhase - len(colourGradients) - 1))) * p
+                xB = centers[firework][0]
+                yB = h - ((h - centers[firework][1]) // (maxFireworkPhase - len(colourGradients) - 2)) * p
 
                 colourIndex = fireworkPhase - p
                 if colourIndex >= len(colourGradients):
                     break
-                if yB == centers[letter][1]:
+                if yB == centers[firework][1]:
                     colourIndex = 0
                 colour = colourGradients[colourIndex]
 
@@ -81,14 +77,13 @@ def draw(pointList, centers):
                 else:
                     pygame.draw.circle(screen, colour, (xB, yB), random.randint(1, 3))
             elif state == "Exploding":
-                for i in range(len(pointList[letter])):
-                    pointP = pointList[letter][i]
-                    distancePCenter = math.sqrt(
-                        (centers[letter][0] - pointP[0]) ** 2 + (centers[letter][1] - pointP[1]) ** 2)
+                for i in range(len(pointList[firework])):
+                    pointP = pointList[firework][i]
+                    distancePCenter = randomRadiusList[i]
                     distanceBCenter = distancePCenter / (maxFireworkPhase - len(colourGradients) - 1) * p
-                    anglePCenter = math.atan2(pointP[1] - centers[letter][1], pointP[0] - centers[letter][0])
-                    xB = int(distanceBCenter * math.cos(anglePCenter)) + centers[letter][0]
-                    yB = int(distanceBCenter * math.sin(anglePCenter)) + centers[letter][1]
+                    anglePCenter = math.atan2(pointP[1] - centers[firework][1], pointP[0] - centers[firework][0])
+                    xB = int(distanceBCenter * math.cos(anglePCenter)) + centers[firework][0]
+                    yB = int(distanceBCenter * math.sin(anglePCenter)) + centers[firework][1]
                     colourIndex = fireworkPhase - p
                     if colourIndex >= len(colourGradients):
                         break
@@ -111,16 +106,21 @@ def draw(pointList, centers):
 while not state == "End":
     startTime = time.time()
     if state == "Setup":
-        pygame.draw.circle(screen, 'white', centers[0], r)
-        pointList, numberOfPoints = findPoints(centers, r)
+        for i in range(len(centerList)):
+            pygame.draw.circle(screen, 'white', centerList[i], radiusList[i], circleThickness)
+        pointList, numberOfPoints = findCirclePoints(centerList, radiusList)
         screen.fill(backgroundColour)
 
         print(numberOfPoints, numberOfPoints * len(colourGradients), sep=", ")
         state = "Rising"
         fireworkPhase = 0
+
+        randomRadiusList = []
+        for i in range(len(pointList[0])):
+            randomRadiusList.append(radiusList[0] - random.randint(0, 150))
     elif state == "Rising" or state == "Exploding":
         screen.fill(backgroundColour)
-        draw(pointList, centers)
+        draw(pointList, centerList)
         fireworkPhase += 1
         if fireworkPhase == maxFireworkPhase:
             if state == "Rising":
@@ -136,19 +136,21 @@ while not state == "End":
             numberOfClicks += 1
 
     if numberOfClicks > 0 and state == "Pause":
-        state = "Rising"
+        state = "Setup"
         numberOfClicks -= 1
 
     pygame.display.flip()
 
-    executionTime = time.time() - startTime
-    if state == "Rising" or state == "Exploding":
-        totalExecutionTime += executionTime
-        if fireworkPhase + len(colourGradients) >= maxFireworkPhase:
-            time.sleep(0.02)
+    if state == "Rising":
         time.sleep(0.01)
-    elif totalExecutionTime != 0:
+    elif state == "Exploding" and fireworkPhase + len(colourGradients) >= maxFireworkPhase:
+        time.sleep(0.08)
+    time.sleep(0.01)
+
+    if totalExecutionTime != 0 and state == "Pause":
         print(totalExecutionTime)
         totalExecutionTime = 0
-
-    # pygame.time.Clock.tick(40)
+    elif state != "Pause":
+        executionTime = time.time() - startTime
+        totalExecutionTime += executionTime
+        startTime = time.time()
