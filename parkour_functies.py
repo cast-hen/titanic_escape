f8ad295bfc36824cb7ba9321cfa840d219620216
12916 <12916@hageveld.nl>
2025-03-25 12:52:40 +0100

    code georganiseerd en randomness vallende blokken toegevoegd

--- parkour_functies.py
+++ parkour_functies.py
@@ -1,6 +1,7 @@
 from pauze import *
 from common import *
 import pygame
+import random
 from pygame import RESIZABLE
 import time
 gravity = 0.6
@@ -81,25 +82,29 @@ class Objects:
         self.Rect = pygame.draw.rect(surface, self.color, (self.xpos - CameraPosx, self.ypos, self.width, self.height))
 
 class MoveObject:
-    def __init__(self, StartPos, EndPos, Speed, WaitTime, Teleport):
+    def __init__(self, StartPos, EndPos, Speed, WaitTime, Teleport, Randomness):
         self.StartPos = StartPos
         self.EndPos = EndPos
         self.Speed = 100 / Speed
         self.WaitTime = WaitTime
         self.Teleport = Teleport
+        self.Randomness = Randomness
+
+        self.randomNumber = 0
 
         self.xDirection = self.EndPos[0] - self.StartPos[0]
         self.yDirection = self.EndPos[1] - self.StartPos[1]
         self.Direction = (self.xDirection / self.Speed, self.yDirection / self.Speed)
 
     def Move(self, pos):
-
+        if self.randomNumber == 0:
+            self.randomNumber = random.randint(-self.Randomness, self.Randomness)
         if pos == self.StartPos:
             xDirection = self.EndPos[0] - self.StartPos[0]
             yDirection = self.EndPos[1] - self.StartPos[1]
             self.Direction = (xDirection / self.Speed, yDirection / self.Speed)
 
-        if 8 > (self.EndPos[0] - pos[0]) + (self.EndPos[1] - pos[1]) > -8 :
+        if 8 > (self.EndPos[0] - pos[0] + self.randomNumber) + (self.EndPos[1] - pos[1]) > -8 :
             if self.Teleport:
                 endpos = self.EndPos
                 self.EndPos = self.StartPos
@@ -107,8 +112,12 @@ class MoveObject:
                 xDirection = self.EndPos[0] - self.StartPos[0]
                 yDirection = self.EndPos[1] - self.StartPos[1]
                 self.Direction = (xDirection / self.Speed, yDirection / self.Speed)
+                self.randomNumber = 0
+
             else:
-                pos = self.StartPos
+                self.randomNumber = random.randint(-self.Randomness, self.Randomness)
+                pos = (self.StartPos[0]  + self.randomNumber, self.StartPos[1])
+
 
 
         TargetPos = pos[0] + self.Direction[0], pos[1] + self.Direction[1]
@@ -128,7 +137,7 @@ def parkour(player):
     jump_height = -25
     speed = 10
     running = True
-    scene = 1
+    scene = 12
     mouseDown = False
     CameraPosx = 0
     RespawnPos = (-900, 450)
@@ -185,18 +194,42 @@ def parkour(player):
     cube39 = Objects(625, 500, 430, 250, 'black', 1, 0, 0, 10, "Collider")
     cube40 = Objects(1025, 280, 400, 475, 'black', 1, 0, 0, 10, "Collider")
 
-    cube41 = Objects(460, 0, 60, 60, 'red', 1, 0, 0, 10, MoveObject((460, -100), (460, 2000), 0.5, 10, False))
-    cube42 = Objects(1050, 600, 60, 60, 'red', 1, 0, 0, 10, MoveObject((1050, -100), (1050, 2000), 0.4, 10, False))
-    cube43 = Objects(150, 300, 60, 60, 'red', 1, 0, 0, 10, MoveObject((150, -100), (150, 2000), 0.6, 10, False))
+    cube41 = Objects(460, 0, 60, 60, 'red', 1, 0, 0, 10, MoveObject((460, -100), (460, 2000), 0.5, 10, False, 0))
+    cube42 = Objects(1050, 600, 60, 60, 'red', 1, 0, 0, 10, MoveObject((1050, -100), (1050, 2000), 0.4, 10, False, 0))
+    cube43 = Objects(150, 300, 60, 60, 'red', 1, 0, 0, 10, MoveObject((150, -100), (150, 2000), 0.6, 10, False, 0))
 
     cube44 = Objects(-500, 285, 400, 600, 'black', 1, 0, 0, 11, "Collider")
     cube45 = Objects(216, 400, 475, 370, 'black', 1, 0, 0, 11, "Collider")
+    cube48 = Objects(1050, 500, 350, 300, 'black', 1, 0, 0, 11, "Collider")
 
-    cube46 = Objects(0, -100, 60, 60, 'red', 1, 0, 0, 11, MoveObject((0, -100), (0, 2000), 1, 10, False))
-    cube47 = Objects(50, -250, 60, 60, 'red', 1, 0, 0, 11, MoveObject((50, -100), (50, 2000), 1, 10, False))
+    cube46 = Objects(0, -100, 60, 60, 'red', 1, 0, 0, 11, MoveObject((0, -100), (0, 2000), 1, 10, False, 0))
+    cube47 = Objects(50, -250, 60, 60, 'red', 1, 0, 0, 11, MoveObject((50, -100), (50, 2000), 1, 10, False, 0))
+    cube49 = Objects(790, -100, 150, 150, 'red', 1, 0, 0, 11, MoveObject((790, -100), (790, 2000), 0.8, 10, False, 0))
+    cube50 = Objects(800, 1000, 150, 150, 'red', 1, 0, 0, 11, MoveObject((790, -100), (790, 2000), 0.8, 10, False, 0))
+
+    cube51 = Objects(-500, 505, 350, 300, 'black', 1, 0, 0, 12, "Collider")
+    cube52 = Objects(-160, 575, 600, 250, 'black', 1, 0, 0, 12, "Collider")
+    cube53 = Objects(285, 480, 540, 290, 'black', 1, 0, 0, 12, "Collider")
+    cube54 = Objects(335, 0, 150, 350, 'black', 1, 0, 0, 12, "Collider")
+    cube55 = Objects(810, 520, 560, 250, 'black', 1, 0, 0, 12, "Collider")
+
+    cube56 = Objects(-500, 1000, 50, 50, 'red', 1, 0, 0, 12, MoveObject((400, -100), (400, 2000), 0.63, 10, False, 900))
+    cube57 = Objects(-500, 1000, 50, 50, 'red', 1, 0, 0, 12, MoveObject((400, -100), (400, 2000), 0.73, 10, False, 900))
+    cube58 = Objects(-500, 1000, 50, 50, 'red', 1, 0, 0, 12, MoveObject((400, -100), (400, 2000), 0.5, 10, False, 900))
+    cube59 = Objects(-500, 1000, 50, 50, 'red', 1, 0, 0, 12, MoveObject((400, -100), (400, 2000), 0.8, 10, False, 900))
+    cube60 = Objects(-500, 1000, 50, 50, 'red', 1, 0, 0, 12, MoveObject((400, -100), (400, 2000), 0.75, 10, False, 900))
+    cube61 = Objects(-500, 1000, 50, 50, 'red', 1, 0, 0, 12, MoveObject((400, -100), (400, 2000), 0.7, 10, False, 900))
+    cube62 = Objects(-500, 1000, 50, 50, 'red', 1, 0, 0, 12, MoveObject((400, -100), (400, 2000), 0.55, 10, False, 900))
+    cube63 = Objects(-500, 1000, 50, 50, 'red', 1, 0, 0, 12, MoveObject((400, -100), (400, 2000), 0.65, 10, False, 900))
 
     # voeg hier nieuwe platformen to zodat ze collision krijgen.
-    platforms = [cube1, cube2, cube3, cube4, cube5, cube6, cube7, cube8, cube9, cube10, cube11, cube12, cube13, cube14, cube15, cube16, cube17, cube18, cube19, cube20, cube21, cube22, cube23, cube24, cube25, cube26, cube27, cube28, cube29, cube30, cube31, cube32, cube33, cube34, cube35, cube36, cube37, cube38, cube39, cube40, cube41, cube42, cube43, cube44, cube45, cube46, cube47]
+    platforms = [cube1, cube2, cube3, cube4, cube5, cube6, cube7, cube8, cube9, cube10,
+                 cube11, cube12, cube13, cube14, cube15, cube16, cube17, cube18, cube19, cube20,
+                 cube21, cube22, cube23, cube24, cube25, cube26, cube27, cube28, cube29, cube30,
+                 cube31, cube32, cube33, cube34, cube35, cube36, cube37, cube38, cube39, cube40,
+                 cube41, cube42, cube43, cube44, cube45, cube46, cube47, cube48, cube49, cube50,
+                 cube51, cube52, cube53, cube54, cube55, cube56, cube57, cube58, cube59, cube60,
+                 cube61, cube62, cube63]
 
     PlayerPos2 = (playerObject.xpos, playerObject.ypos)
     PlayerPos1 = PlayerPos2
@@ -214,6 +247,7 @@ def parkour(player):
         screen.fill((135, 206, 250))
         Colliders = playerObject.update_pos(platforms, CameraPosx, scene)
 
+        #Collision glitch fix
         if CollisionGlitch:
 
             if Afstand(PlayerPos1, PlayerPos2) < 50 or Afstand(PlayerPos1, PlayerPos2) > 500 :
@@ -230,98 +264,65 @@ def parkour(player):
 
 
         #spawnt alle objects
+
+        for platform in platforms:
+            if platform.ObjectScene == scene:
+                platform.draw(screen, CameraPosx)
+            if type(platform.Type) == MoveObject:
+                (platform.xpos, platform.ypos) = platform.Type.Move((int(platform.xpos), int(platform.ypos)))
         if scene == 1:
             RespawnPos = (270, 450)
             playerObject.draw(screen, CameraPosx)
-            cube1.draw(screen, CameraPosx)
-            cube10.draw(screen, CameraPosx)
-            cube2.draw(screen, CameraPosx)
-            cube3.draw(screen, CameraPosx)
 
             cube16.draw(screen, CameraPosx)
         if scene == 2:
             RespawnPos = (-35, 400)
             playerObject.draw(screen, CameraPosx)
-            cube4.draw(screen, CameraPosx)
-            cube6.draw(screen, CameraPosx)
-            cube5.draw(screen, CameraPosx)
-            cube7.draw(screen, CameraPosx)
-            cube8.draw(screen, CameraPosx)
-            print(CameraPosx)
+
         if scene == 3:
             scene += 1
         if scene == 4:
             RespawnPos = (-170, 500)
             playerObject.draw(screen, CameraPosx)
-            cube9.draw(screen, CameraPosx)
-            cube11.draw(screen, CameraPosx)
-            cube12.draw(screen, CameraPosx)
-            cube13.draw(screen, CameraPosx)
-            cube14.draw(screen, CameraPosx)
-            cube15.draw(screen, CameraPosx)
+
         if scene == 5:
             RespawnPos = (-175, 370)
             playerObject.draw(screen, CameraPosx)
-            cube17.draw(screen, CameraPosx)
-            cube18.draw(screen, CameraPosx)
-            cube19.draw(screen, CameraPosx)
-            cube20.draw(screen, CameraPosx)
-            cube21.draw(screen, CameraPosx)
-            cube22.draw(screen, CameraPosx)
-            cube23.draw(screen, CameraPosx)
+
         if scene == 6:
             RespawnPos = (-380, 385)
             playerObject.draw(screen, CameraPosx)
-            cube24.draw(screen, CameraPosx)
-            cube25.draw(screen, CameraPosx)
-            cube26.draw(screen, CameraPosx)
-            cube27.draw(screen, CameraPosx)
-            cube28.draw(screen, CameraPosx)
+
         if scene == 7:
             RespawnPos = (-340, 350)
             playerObject.draw(screen, CameraPosx)
-            cube29.draw(screen, CameraPosx)
-            cube30.draw(screen, CameraPosx)
-            cube31.draw(screen, CameraPosx)
+
         if scene == 8:
             RespawnPos = (-340, 350)
             playerObject.draw(screen, CameraPosx)
-            cube32.draw(screen, CameraPosx)
-            cube33.draw(screen, CameraPosx)
-            cube34.draw(screen, CameraPosx)
-            cube35.draw(screen, CameraPosx)
+
         if scene == 9:
             LevelGehaald()
             scene += 1
             player.lives = 5
         if scene == 10:
             RespawnPos = (-340, 350)
-
-            cube41.draw(screen, CameraPosx)
-            cube42.draw(screen, CameraPosx)
-            cube43.draw(screen, CameraPosx)
-
             playerObject.draw(screen, CameraPosx)
-            cube36.draw(screen, CameraPosx)
-            cube37.draw(screen, CameraPosx)
-            cube38.draw(screen, CameraPosx)
-            cube39.draw(screen, CameraPosx)
-            cube40.draw(screen, CameraPosx)
 
         if scene == 11:
-            RespawnPos = (-340, 350)
-
-            cube46.draw(screen, CameraPosx)
-            cube47.draw(screen, CameraPosx)
+            RespawnPos = (-340, 150)
 
             playerObject.draw(screen, CameraPosx)
-            cube44.draw(screen, CameraPosx)
-            cube45.draw(screen, CameraPosx)
-
-
-
-
         if scene == 12:
+            RespawnPos = (-340, 400)
+
+            playerObject.draw(screen, CameraPosx)
+
+
+
+
+
+        if scene == 13:
             eind()
             return "Menu"
 
@@ -342,14 +343,6 @@ def parkour(player):
             #returns enemy waarmee je collide
             if type(Collider) == enemy:
                 return Collider
-
-
-
-        (cube41.xpos, cube41.ypos) = cube41.Type.Move((int(cube41.xpos), int(cube41.ypos)))
-        (cube42.xpos, cube42.ypos) = cube42.Type.Move((int(cube42.xpos), int(cube42.ypos)))
-        (cube43.xpos, cube43.ypos) = cube43.Type.Move((int(cube43.xpos), int(cube43.ypos)))
-        (cube46.xpos, cube46.ypos) = cube46.Type.Move((int(cube46.xpos), int(cube46.ypos)))
-        (cube47.xpos, cube47.ypos) = cube47.Type.Move((int(cube47.xpos), int(cube47.ypos)))
 
 
         # veranderd camera position
